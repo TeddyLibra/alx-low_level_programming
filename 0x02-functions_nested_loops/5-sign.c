ff8aa71434fd60416162859c5856005802bcf051
TeddyLibra <teddyg2comp@gamil.com>
2023-03-21 03:13:16 -0700

    All question from Functions, nested loops project

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -8,19 +7,19 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar(43);
-		return (1);
-	} 
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	} 
-	else
-	{
-     		_putchar(48);
-		return (0);
-		}
+        if (n > 0)
+        {
+        _putchar(43);
+        return (1);
+        }
+        else if (n < 0)
+        {
+        _putchar(45);
+        return (-1);
+        }
+        else
+        {
+        _putchar(48);
+        return (0);
+        }
 }
