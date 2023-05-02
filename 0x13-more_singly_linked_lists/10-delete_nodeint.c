cf656c4d14109f5dbd00bfda93e2b13170e0908d
TeddyLibra <teddyg2comp@gmail.com>
2023-05-02 12:35:40 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,40 +1,40 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - to delete node
- * @head_v: point to 2d 1st element
- * @index_delete: node 2b deleted
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
  *
- * Return: if success 1 else -1
+ * Return: 1 (Success), or -1 (Fail)
  */
-int delete_nodeint_at_index(listint_t **head_v, unsigned int index_delete)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temporary = *head_v;
-	listint_t *this_var = NULL;
-	unsigned int x = 0;
+	listint_t *temp = *head;
+	listint_t *current = NULL;
+	unsigned int i = 0;
 
-	if (*head_v == NULL)
+	if (*head == NULL)
 		return (-1);
 
-	if (index_delete == 0)
+	if (index == 0)
 	{
-		*head_v = (*head_v)->next;
-		free(temporary);
+		*head = (*head)->next;
+		free(temp);
 		return (1);
 	}
 
-	while (x < index_delete - 1)
+	while (i < index - 1)
 	{
-		if (!temporary || !(temporary->next))
+		if (!temp || !(temp->next))
 			return (-1);
-		temporary = temporary->next;
-		x++;
+		temp = temp->next;
+		i++;
 	}
 
 
-	this_var = temporary->next;
-	temporary->next = this_var->next;
-	free(this_var);
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
 
 	return (1);
 }
